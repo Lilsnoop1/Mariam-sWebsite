51b4963c51bbab1483fc83a0137b7fa2784c37c3
Ammar Zulfiqar <ammarzulfiqar@Ammars-MacBook-Air.local>
2023-10-21 22:23:52 +0500

    Updated Website

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb+srv://ammar:ammar@cluster0.vuzjvhx.mongodb.net/mariamDB");
 
 const contactlistSchema = mongoose.Schema({
         title : String,
@@ -20,17 +21,17 @@ app.get("/contact",(req,res)=>{
 })
 app.post("/contact",(req,res)=>{
     res.render("contact.ejs");
-    // const infoitem = new marieInfo({
-    //     title: req.body.title,
-    //     text: req.body.text
-    // })
-    // marieInfo.insertMany(infoitem)
-    //     .then(result =>{
-    //         console.log(result);
-    //     })
-    //     .catch(err =>{
-    //         console.log(err);
-    //     })
+    const infoitem = new marieInfo({
+        title: req.body.title,
+        text: req.body.text
+    })
+    marieInfo.insertMany(infoitem)
+        .then(result =>{
+            console.log(result);
+        })
+        .catch(err =>{
+            console.log(err);
+        })
 })
 app.get("/painting",(req,res)=>{
     res.render("painting.ejs");
@@ -39,8 +40,8 @@ app.get("/about",(req,res)=>{
     res.render("about.ejs");
 })
 app.get("/digital",(req,res)=>{
-    res.send("this baby is under construction!")
+    res.render("digitalart.ejs");
 })
-app.listen(process.env.PORT,()=>{
+app.listen(3000,()=>{
     console.log("Server started on port 3000");
 })
